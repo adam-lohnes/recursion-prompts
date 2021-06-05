bb15256583e841a5bbadd4e5071ed6d84a11f527
Adam <adam.a.lohnes@gmail.com>
2021-06-04 20:23:49 -0700

    Implement letterTally()

--- src/recursion.js
+++ src/recursion.js
@@ -616,8 +616,24 @@ var flatten = function(array) {
 // 31. Given a string, return an object containing tallies of each letter.
 // letterTally('potato'); // {p:1, o:2, t:2, a:1}
 var letterTally = function(str, obj) {
-  // base case ->
-  // recursive case ->
+  // edge case -> obj not initialized or defined
+  if ( obj === undefined ) {
+    obj = {};
+  }
+  // base case -> str is empty string
+  if ( str === '' ) {
+    return;
+  }
+
+  if ( obj[str[0]] === undefined ) {
+    obj[str[0]] = 0;
+  }
+  obj[str[0]] += 1;
+
+  // recursive case -> add first letter of str to obj
+  nextLetter = letterTally(str.slice(1), obj);
+
+  return obj;
 };
 
 // 32. Eliminate consecutive duplicates in a list. If the list contains repeated
